2980031fb43f2dde7011e9f5a61275c7c6741096
Lucas Lino <lucasmizukawa@gmail.com>
2025-12-17 00:52:19 +0000

    adds `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,34 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST To /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+
+  const response1body = await response1.json();
+
+  expect(Array.isArray(response1body)).toBe(true);
+  expect(response1body.length).toBeGreaterThan(0);
+  expect(response1body[0].name).toBe("1765925517646_test-migration");
+  expect(response1body[0].path).toBe(
+    "infra/migrations/1765925517646_test-migration.js",
+  );
+  expect(response1body[0].timestamp).toBe(1765925517646);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2body = await response2.json();
+
+  expect(Array.isArray(response2body)).toBe(true);
+  expect(response2body.length).toBe(0);
+});
